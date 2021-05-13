5fb1242b473276549190b2041b6744c2ebe49004
LukeHalby <lukehalby@gmail.com>
2021-05-13 12:33:58 +1000

    Updates

---
+++ precache-manifest.c4537c3e1abb604c333d9621340f86b4.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5009d8ec480725b3eea396a63e282729",
+    "url": "/StatsExpressPages/index.html"
+  },
+  {
+    "revision": "c9090bc72356c37462c6",
+    "url": "/StatsExpressPages/static/css/3.7210cd4b.chunk.css"
+  },
+  {
+    "revision": "0923f2247fa0ae99920f",
+    "url": "/StatsExpressPages/static/css/4.4fd21cf9.chunk.css"
+  },
+  {
+    "revision": "eb8353f81908442806b7",
+    "url": "/StatsExpressPages/static/js/2.88e892c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StatsExpressPages/static/js/2.88e892c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9090bc72356c37462c6",
+    "url": "/StatsExpressPages/static/js/3.deb77282.chunk.js"
+  },
+  {
+    "revision": "0923f2247fa0ae99920f",
+    "url": "/StatsExpressPages/static/js/4.ad088617.chunk.js"
+  },
+  {
+    "revision": "d356f41dac612bcd5c3c",
+    "url": "/StatsExpressPages/static/js/main.e97370aa.chunk.js"
+  },
+  {
+    "revision": "d138d77ed5f939e19808",
+    "url": "/StatsExpressPages/static/js/runtime-main.8db95e49.js"
+  },
+  {
+    "revision": "670c2710eb4638b9e481997a249cc68a",
+    "url": "/StatsExpressPages/static/media/Ares.670c2710.svg"
+  },
+  {
+    "revision": "f74c131457300004adac0b427c91815f",
+    "url": "/StatsExpressPages/static/media/Ascent.f74c1314.svg"
+  },
+  {
+    "revision": "9df3e56ac965e7771b4b24c058ea047a",
+    "url": "/StatsExpressPages/static/media/Bind.9df3e56a.svg"
+  },
+  {
+    "revision": "e50a416a66aebaa7f4a90764eeaf58d8",
+    "url": "/StatsExpressPages/static/media/Brimstone.e50a416a.svg"
+  },
+  {
+    "revision": "a8cd4e7a9adc0b0f33204d4dd4963fe4",
+    "url": "/StatsExpressPages/static/media/Bucky.a8cd4e7a.svg"
+  },
+  {
+    "revision": "ea27bc2ef14b947d8162c19a77b4d53b",
+    "url": "/StatsExpressPages/static/media/Bulldog.ea27bc2e.svg"
+  },
+  {
+    "revision": "c5117527a83a97e6b5ada10e785e1c62",
+    "url": "/StatsExpressPages/static/media/ChangeArrows.c5117527.svg"
+  },
+  {
+    "revision": "a2409427d6f2cd0f78e5728a84c0b2ee",
+    "url": "/StatsExpressPages/static/media/Classic.a2409427.svg"
+  },
+  {
+    "revision": "2f17f2fd541ce692576a7f32f732f254",
+    "url": "/StatsExpressPages/static/media/Cypher.2f17f2fd.svg"
+  },
+  {
+    "revision": "3418913900ea00cd0b2549d1de13ab3c",
+    "url": "/StatsExpressPages/static/media/DownArrow.34189139.svg"
+  },
+  {
+    "revision": "7eecb8cf4a35bba20888af6339af8158",
+    "url": "/StatsExpressPages/static/media/Frenzy.7eecb8cf.svg"
+  },
+  {
+    "revision": "e7ac85f82f4620611317b81e7e63924a",
+    "url": "/StatsExpressPages/static/media/Ghost.e7ac85f8.svg"
+  },
+  {
+    "revision": "debd4d445150f8c8aa61f349aac95124",
+    "url": "/StatsExpressPages/static/media/Graphs.debd4d44.svg"
+  },
+  {
+    "revision": "02621a98e1b3ff85d28d9f9fdc9d5e7a",
+    "url": "/StatsExpressPages/static/media/Guardian.02621a98.svg"
+  },
+  {
+    "revision": "777a744109e76fc0bd50e0af65950599",
+    "url": "/StatsExpressPages/static/media/Haven.777a7441.svg"
+  },
+  {
+    "revision": "0d64dfafa749f9da241ffc7b10b71bf3",
+    "url": "/StatsExpressPages/static/media/IdCards.0d64dfaf.svg"
+  },
+  {
+    "revision": "3649888a0c01a84fba68731f1e0ccaa3",
+    "url": "/StatsExpressPages/static/media/Jett.3649888a.svg"
+  },
+  {
+    "revision": "76581a38ba0c62956cdd4585300df48b",
+    "url": "/StatsExpressPages/static/media/Judge.76581a38.svg"
+  },
+  {
+    "revision": "5d31ed2329187612042180aa8a5c2af6",
+    "url": "/StatsExpressPages/static/media/Marshal.5d31ed23.svg"
+  },
+  {
+    "revision": "8711e67fc2db9a8d63ad5f45f200883d",
+    "url": "/StatsExpressPages/static/media/Melee.8711e67f.svg"
+  },
+  {
+    "revision": "501ffef6e5468d138f665569c3b013bb",
+    "url": "/StatsExpressPages/static/media/NotFound.501ffef6.svg"
+  },
+  {
+    "revision": "c2cb856b4f7f2f6cee5d9317ba9821b4",
+    "url": "/StatsExpressPages/static/media/NullArrow.c2cb856b.svg"
+  },
+  {
+    "revision": "f4435458cc06675a1f9ef7f0b48cd5eb",
+    "url": "/StatsExpressPages/static/media/Odin.f4435458.svg"
+  },
+  {
+    "revision": "b6f9fe80a6145837281bf4fab7a0e8f1",
+    "url": "/StatsExpressPages/static/media/Omen.b6f9fe80.svg"
+  },
+  {
+    "revision": "bd0b4556fa845fd00c11b769cad00a64",
+    "url": "/StatsExpressPages/static/media/Operator.bd0b4556.svg"
+  },
+  {
+    "revision": "67d35dd362f6082a243e802dee7302c3",
+    "url": "/StatsExpressPages/static/media/Panels.67d35dd3.svg"
+  },
+  {
+    "revision": "a4c89df4dae56d74829ebc5b6a6167c4",
+    "url": "/StatsExpressPages/static/media/ParallaxCharacters.a4c89df4.svg"
+  },
+  {
+    "revision": "ba4a96f64c88d5773221f0231a0c9ad7",
+    "url": "/StatsExpressPages/static/media/ParallaxForeground.ba4a96f6.svg"
+  },
+  {
+    "revision": "2eb40d768b01910764e19a545cd05ef2",
+    "url": "/StatsExpressPages/static/media/Phantom.2eb40d76.svg"
+  },
+  {
+    "revision": "7ac8eb6681dfdc334cea4c7177aa9b66",
+    "url": "/StatsExpressPages/static/media/Phoenix.7ac8eb66.svg"
+  },
+  {
+    "revision": "46e4613e3d50f29977fd120d9f2ac34f",
+    "url": "/StatsExpressPages/static/media/Radiant.46e4613e.svg"
+  },
+  {
+    "revision": "ae2ab3b8976327a60f42532211aa027f",
+    "url": "/StatsExpressPages/static/media/Raze.ae2ab3b8.svg"
+  },
+  {
+    "revision": "264d0f1e3dc807569929fc04be4069ee",
+    "url": "/StatsExpressPages/static/media/Reyna.264d0f1e.svg"
+  },
+  {
+    "revision": "70844ababa42ad8bdf3ab25fb6920c43",
+    "url": "/StatsExpressPages/static/media/Sage.70844aba.svg"
+  },
+  {
+    "revision": "dba77e7b1e5375ebc8035fa1d592430f",
+    "url": "/StatsExpressPages/static/media/Sheriff.dba77e7b.svg"
+  },
+  {
+    "revision": "97ab66a1f27858b77ed70b5659906bd5",
+    "url": "/StatsExpressPages/static/media/Shorty.97ab66a1.svg"
+  },
+  {
+    "revision": "ad6df884ca1d5d70a6b0294b21b0f591",
+    "url": "/StatsExpressPages/static/media/Sova.ad6df884.svg"
+  },
+  {
+    "revision": "ffdf942187f6ff242c5b11dcd3ffbdd4",
+    "url": "/StatsExpressPages/static/media/Spectre.ffdf9421.svg"
+  },
+  {
+    "revision": "9239420905ef39cde81d644a2fe7ac2b",
+    "url": "/StatsExpressPages/static/media/Split.92394209.svg"
+  },
+  {
+    "revision": "cb58b4d32255f1fb0b703c076c43329b",
+    "url": "/StatsExpressPages/static/media/StatsExpress.cb58b4d3.svg"
+  },
+  {
+    "revision": "6a99086195036e90e62423523a9a8b4f",
+    "url": "/StatsExpressPages/static/media/Stinger.6a990861.svg"
+  },
+  {
+    "revision": "4fcd26e3a13bdf730f6f99953d3437d5",
+    "url": "/StatsExpressPages/static/media/UpArrow.4fcd26e3.svg"
+  },
+  {
+    "revision": "bd83cfd449d3fcce8f467d73d216bec8",
+    "url": "/StatsExpressPages/static/media/Vandal.bd83cfd4.svg"
+  },
+  {
+    "revision": "62477c5ef5cc5a58be185a997546465d",
+    "url": "/StatsExpressPages/static/media/Viper.62477c5e.svg"
+  },
+  {
+    "revision": "9b9f722d15133f11a3b160076a64eb00",
+    "url": "/StatsExpressPages/static/media/WidgetSwap.9b9f722d.svg"
+  },
+  {
+    "revision": "94fbc99887b816dfa1bdbd434b987935",
+    "url": "/StatsExpressPages/static/media/breach.94fbc998.svg"
+  },
+  {
+    "revision": "7b5f60546ca64e7fca494cbdcd1e7c31",
+    "url": "/StatsExpressPages/static/media/redX.7b5f6054.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc15f733e12d2a68b61308c8b09321de.js"
+  "/StatsExpressPages/precache-manifest.c4537c3e1abb604c333d9621340f86b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/StatsExpressPages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
